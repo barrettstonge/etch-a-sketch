128bf1a5c3ec1759ce8af49c114f8ac34a7a8c70
barrettstonge <238751644+barrettstonge@users.noreply.github.com>
2026-09-23 12:51:40 -0400

    created an event listener for enter button to announce users input, clear out the previous conatiner divs and rerun function to create new squares with users input

--- script.js
+++ script.js
@@ -6,25 +6,73 @@ button.innerHTML = "HIT ME!";
 button.classList.add("button");
 body.appendChild(button);
 
+const textInput = document.createElement("input");
+textInput.id = "myTextInput";
+textInput.type = "text";
+//textInput.placeholder = "1-100";
+textInput.value = "";
+textInput.max = "100";
+textInput.min = "1";
+body.appendChild(textInput);
+
+button.addEventListener("click",()=>{
+    window.alert("Input a number (1-100) and the board with adjust how many blocks you have to sketch with.");
+});
+
+const enter = document.createElement("button");
+enter.innerHTML = "ENTER";
+enter.classList.add("enter");
+body.appendChild(enter);
+
+enter.addEventListener("click", ()=>{
+    
+    let inputField = document.getElementById("myTextInput");
+    
+    let inputValue = inputField.value;
+
+    
+        if(inputValue<=100 && inputValue >=1){ 
+             container.innerHTML = '';
+             window.alert("You entered, " + inputValue);
+             createGrid(inputValue);
+            
+        
+
+
+ };
+
+ 
+   
+});
+  
+let inputField = document.getElementById("myTextInput");
+let inputValue = inputField.value;
 
 const container = document.getElementById("container");
 
-function createGrid() {
-  const totalSquares = 16 * 16; 
+function createGrid(inputValue) {
+  
 
+let totalSquares = inputValue * inputValue; 
+percentage = 100/inputValue;
   for (let i = 0; i < totalSquares; i++) {
     const square = document.createElement("div");
+    square.style.width = `${percentage}%`;
+      square.style.height = `${percentage}%`;
     square.classList.add("grid-square");
     square.addEventListener("mouseenter", () => {
-      square.style.backgroundColor = "#333";
+      square.style.backgroundColor = "teal";
     });
     square.addEventListener("mouseleave", ()=> {
-        square.style.backgroundColor = "orange";
+        square.style.backgroundColor = "pink";
     });
       container.appendChild(square);
-  }
-}
-createGrid();
+  };
+};
+createGrid(12);
+
+
+
 
 
 
